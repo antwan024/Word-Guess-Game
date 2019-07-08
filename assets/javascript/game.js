9172eec1d5d6b708f9fd092960a206670aedc4bd
Anthony Nguyen <a.nguyen525i@gmail.com>
2019-07-08 14:13:55 -0600

    Got counters and letter checket working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,32 +2,80 @@ var carList = ['Ferrari', 'Honda', 'Lexus', 'Mercedes', 'BMW', 'Acura', 'Lamborg
 var blankWord = [];
 var randomWord = "";
 var randomWordLetters = [];
+var guessesLeft = 10;
+var usedLetters = [];
+
+
 
 var pickWord = function() {
     randomIndex = Math.floor(Math.random() * carList.length);
-    randomWord = carList[randomIndex];
+    randomWord = carList[randomIndex].toLowerCase();
     randomWordLetters = randomWord.split("");
 
-    // alert(randomWord);
-    // alert(randomWordLetters);
+    
+};
+
+var startGame = function() {
+
+    pickWord();
+    guessesLeft = 10;
+
+    for(var i = 0; i < randomWordLetters.length; i++){
+        blankWord.push("_");  
+        
+    };
+
+    alert("Guess this! " + blankWord);
+    alert("Number of letters: " + randomWordLetters.length);
+    alert("Answer: " + randomWord);
+    alert("Array: " + randomWordLetters);
+
+
 
 };
 
-pickWord();
 
-for(var i = 0; i < randomWordLetters.length; i++){
-    blankWord.push("_");    
+var checkLetter = function(playerLetter) {
+
+    var isCorrect = true;
+    var correctCounter = 0;
+    
+    alert("Your guess is " + playerLetter + " !");
+
+
+    for(var i=0; i<randomWord.length; i++){
+
+        if(randomWordLetters[i] === playerLetter) {
+            blankWord[i] = playerLetter;
+            correctCounter++;
+        };
+
+
+
+    };
+
+    if (correctCounter===0){
+        guessesLeft--;
+        alert("You have only have " + guessesLeft + " guess(es) left!");
+    } else{
+        alert("You are correct!");
+    };
+
+
+
+        
+    
+
+   
+
 
 };
 
+
+startGame();
+checkLetter('e');
+checkLetter('a');
 alert(blankWord);
-alert(randomWordLetters.length)
-alert(randomWord);
-alert(randomWordLetters);
-
-
-
-
 
 
 
